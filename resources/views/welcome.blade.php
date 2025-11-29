ab3456f05a44b4c34a1c194385c3d411553f4edd
tioprayudha <tioprayudha07@gmail.com>
2025-11-29 19:54:19 +0700

    slicing example portfolio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Luar Sekolah</title>
 
+    <script src="//unpkg.com/alpinejs" defer></script>
+
     @vite('resources/css/app.css')
 </head>
-<body>
+<>
     <x-headline/>
     <x-programs/>
+    <x-portfolio/>
 </body>
 </html>
